8a5bb07986956e751ff87fe88a600718bea2f9e5
Romain Sertelon <romain@sertelon.fr>
2014-09-30 09:14:21 +0200

    Removes wrong copyright notice

--- spring-dbunit-core/src/main/java/com/excilys/ebi/spring/dbunit/dataset/DataSetDecorator.java
+++ spring-dbunit-core/src/main/java/com/excilys/ebi/spring/dbunit/dataset/DataSetDecorator.java
@@ -1,19 +1,7 @@
-/*
- * Copyright (c) 2001-2014 Group JCDecaux. 
- * 17 rue Soyer, 92523 Neuilly Cedex, France.
- * All rights reserved. 
- * 
- * This software is the confidential and proprietary information
- * of Group JCDecaux ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only
- * in accordance with the terms of the license agreement you
- * entered into with Group JCDecaux.
- */
-
 package com.excilys.ebi.spring.dbunit.dataset;
 
 public interface DataSetDecorator {
     String getStringToReplace();
-    
+
     String getStringReplacement();
 }
